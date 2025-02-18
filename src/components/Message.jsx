786b8112e6a70e5c16f51fc1e2c5812bc1353e47
squaalldev <squaall@gmail.com>
2025-02-18 02:29:39 -0600

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,42 +1,19 @@
-function Message({ message, reactions, onReaction, activeEmojiPicker, onToggleEmojiPicker }) {
-  const messageClassName = `message ${message.isAI ? 'ai-message' : ''}`;
-  
+import React from 'react'
+import { MessageReactions } from './MessageReactions'
+
+export function Message({ message }) {
   return (
-    <div key={message.id} className={messageClassName}>
-      <img 
-        className="message-avatar" 
-        src={message.isAI ? '/Untitled design (21).png' : `https://images.websim.ai/avatar/${message.username}`}
-        alt={message.username} 
-      />
+    <div className={`message ${message.isAI ? 'ai' : 'user'}`}>
       <div className="message-content">
-        <div className="message-header">
-          <span className="message-author">{message.username}</span>
-          <span className="message-time">
-            {new Date(message.created_at).toLocaleTimeString()}
-          </span>
-        </div>
-        {message.content && (
-          <div 
-            className="message-text"
-            dangerouslySetInnerHTML={{ __html: message.content }}
-          />
-        )}
-        {message.imageUrl && (
-          <img 
-            className="message-image" 
-            src={message.imageUrl} 
-            alt="Uploaded content"
-            onClick={() => window.open(message.imageUrl, '_blank')}
-          />
-        )}
-        <MessageReactions 
-          messageId={message.id}
-          reactions={reactions}
-          onReaction={onReaction}
-          activeEmojiPicker={activeEmojiPicker}
-          onToggleEmojiPicker={onToggleEmojiPicker}
-        />
+        <div className="message-text">{message.content}</div>
+        <MessageReactions message={message} />
+      </div>
+      <div className="message-info">
+        <span className="message-username">{message.username}</span>
+        <span className="message-time">
+          {new Date(message.created_at).toLocaleTimeString()}
+        </span>
       </div>
     </div>
-  );
+  )
 }
