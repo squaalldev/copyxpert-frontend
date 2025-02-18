f25913ee21d75231eaba9268ae57137cef23642e
squaalldev <squaall@gmail.com>
2025-02-18 02:14:57 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
-});
+})
